08e04fd85e355c80247e1fd2c3c8f14ff5956be8
Fanny Larasya <larasyaapp@gmail.com>
2025-11-27 15:49:55 +0700

    Add files via upload

---
+++ pertemuan10/bangun_datar.py
@@ -0,0 +1,15 @@
+import math
+def luas_persegi(sisi):
+    return sisi * sisi
+
+def luas_segitiga(alas, tinggi):
+    return 0.5 * alas * tinggi
+
+def luas_lingkaran(jari_jari):
+    return math.pi * jari_jari * jari_jari
+
+def luas_ketupat(diagonal1, diagonal2):
+    return 0.5 * diagonal1 * diagonal2
+
+def luas_jajar_genjang(alas, tinggi):
+    return alas * tinggi
