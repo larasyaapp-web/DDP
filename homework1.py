8aaa62e8bc6a203d3c8b57861f8a3aebcf02fdee
Fanny Larasya <larasyaapp@gmail.com>
2025-12-01 17:17:39 +0700

    Add files via upload

---
+++ homework1.py
@@ -0,0 +1,9 @@
+#meminta nama, umur, jurusan, dan nilai UTS dari pengguna
+#gunakan minimal 4 variable berbeda dan f-string untuk mencetak hasil
+
+nama = "Fanny Larasya Putri"
+umur = 19
+prodi = "Sistem Informasi"
+nilai_UTS= float (90.5)
+
+print(f"""Halo {nama} dari jurusan {prodi}, umur kamu {umur} tahun dan nilai UTS kamu {nilai_UTS}""")
