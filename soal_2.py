a212938246744f085d735bc08582d782ab5e184d
Fanny Larasya <larasyaapp@gmail.com>
2025-12-01 17:15:47 +0700

    Add files via upload

---
+++ soal_2.py
@@ -0,0 +1,13 @@
+# 2. buatlah program python untuk menuliskan profil nama teman (nama,nim,kelas, no telp, alamat) menggunakan variable dan dicetak print
+
+nama = "Dava Rasya Munfariz"
+NIM = "0110125115"
+kelas = "SI02"
+telp = "087858650789"
+domisili = "Depok"
+
+print(nama)
+print(NIM)
+print(kelas)
+print(telp)
+print(domisili)
