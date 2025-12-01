9892878a9538219b4ee12a4fc28cc81f21f03ae0
Fanny Larasya <larasyaapp@gmail.com>
2025-12-01 17:16:52 +0700

    Add files via upload

---
+++ soal_5.py
@@ -0,0 +1,22 @@
+# 5. buat program python untuk mencari luas dan keliling tabung
+
+#luas tabung
+rumus = 2
+pi = float (3.14)
+jari_jari = int(input("masukan jari jari :"))
+tinggi = int(input("masukan tinggi :"))
+
+jumlah1 = jari_jari + tinggi
+jumlah2 = rumus * pi * jari_jari * jumlah1
+
+print (jumlah2)
+
+#keliling tabung
+
+rumus = 2
+pi = float(3.14)
+jari_jari = int(input("masukan jari jari : "))
+
+jumlah = rumus * pi * jari_jari
+
+print(jumlah)
