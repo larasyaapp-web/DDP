f61fce8c59a9751829c306fe3e8d9ca248d1fb4b
Fanny Larasya <larasyaapp@gmail.com>
2025-12-01 17:15:22 +0700

    Add files via upload

---
+++ soal_1.py
@@ -0,0 +1,13 @@
+# 1. buatlah program python untuk menuliskan profil pribadi (nama,nim,kelas, no telp, alamat) menggunakan variable dan dicetak print
+
+nama = "Fanny Larasya Putri"
+NIM = "0110125010"
+kelas = "SI02"
+telp = "089655237866"
+domisili = "Depok"
+
+print(nama)
+print(NIM)
+print(kelas)
+print(telp)
+print(domisili)
