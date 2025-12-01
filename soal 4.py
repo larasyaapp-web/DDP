76ab8e23fdce1126d7ba91bbe10c9eb540c3d0ea
Fanny Larasya <larasyaapp@gmail.com>
2025-12-01 17:16:40 +0700

    Add files via upload

---
+++ soal 4.py
@@ -0,0 +1,8 @@
+# 4.buat program python untuk mencari nilai konversi dari celcius ke fahreinheit
+
+celcius = float(input("Masukan nilai dalam celcius :"))
+fahrenheit = (celcius * 1.8) + 32
+
+print("============ Hasil dari konversi ==============")
+print(f"Nilai dalam celcius adalah    : {celcius} derajat C")
+print(f"Nilai dalam fahrenheit adalah : {fahrenheit} derajat F")
